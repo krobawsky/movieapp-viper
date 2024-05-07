a9f28c79d107b329030df4e41c316b8306b14dd6
Ricardo Berrospi <ricardoberrospi@bcp.com.pe>
2024-05-06 20:30:05 -0500

    Initial Commit

---
+++ moviesapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  moviesapp
+//
+//  Created by Ricardo Daniel Berrospi Quispe on 6/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
